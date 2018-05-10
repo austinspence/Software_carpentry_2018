a499af1e48cf96402f54ff2be4bfc3b67523fcc9
Austin Spence <austin.spence@uconn.edu>
2018-05-10 16:36:47 -0400

    Start software carpentry on git

---
+++ Code/software_carpentry.R
@@ -0,0 +1,29 @@
+#### Software Carpentry 2018
+## May 10th, 2018
+## Austin Spence
+
+setwd("~/Desktop")
+
+jesus <- read.csv("software.csv")
+
+
+## MAKE SOME CATS
+cats <- data.frame(coat = c("red", "blue", "green"),
+                   mass = c(5, 17, 0.3), 
+                   string = c(1, 0, 1))
+cats
+cats[,2]
+cats$mass
+
+
+#### Load in some REAL data
+jesus <- read.csv("/Users/austinspence/Desktop/Software_Carpentry_2018/Data/software.csv")
+
+# Make it only Afghanistan
+afghanistan <- jesus[which(jesus$country == "Afghanistan"),]
+
+afghanistan$gdp <- afghanistan$pop*afghanistan$gdpPercap #make this a variable
+
+write.csv(x = afghanistan, file = "./Results/afghanistan.csv") #write a dumb csv
+
+          
